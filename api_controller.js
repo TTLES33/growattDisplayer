dbbc3fee638a5a31a795d15e263fda5de562a79d
Jan Grygar <jangrygar5@gmail.com>
2025-12-29 20:55:35 +0100

    Version 1.6

--- api_controller.js
+++ api_controller.js
@@ -96,6 +96,10 @@ app.get('/css.css', function(req, res) {
     global.addlog("GET", req.params.tagid);
     res.sendFile(path.join(__dirname, '/growattDisplayerClient/css.css'));
 });
+app.get('/settings.css', function(req, res) {
+    global.addlog("GET", req.params.tagid);
+    res.sendFile(path.join(__dirname, '/growattDisplayerClient/settings.css'));
+});
 app.get('/color_pallete.css', function(req, res) {
     global.addlog("GET", req.params.tagid);
     res.sendFile(path.join(__dirname, '/growattDisplayerClient/color_pallete.css'));
